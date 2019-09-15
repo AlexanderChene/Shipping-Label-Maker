21f49d9f3dd2cfd88cdab89c3f3f1b484af021a9
Yixiao <yixiaozhouchen@hotmail.com>
2019-09-15 16:39:26 -0400

    more state

--- src/core/common/DropdownOptions/stateOptions.js
+++ src/core/common/DropdownOptions/stateOptions.js
@@ -1,6 +1,16 @@
 const options = [
    
     { value: 'AL', label: 'AL' },
+    { value: 'AK', label: 'AK' },
+    { value: 'AZ', label: 'AZ' },
+    { value: 'AR', label: 'AR' },
+    { value: 'CO', label: 'CO' },
+    { value: 'CT', label: 'CT' },
+    { value: 'DE', label: 'DE' },
+    { value: 'FL', label: 'FL' },
+    { value: 'GA', label: 'GA' },
+    { value: 'KS', label: 'KS' },
+    { value: 'KY', label: 'KY' },
     { value: 'NJ', label: 'NJ' },
     { value: 'NY', label: 'NY' },
     { value: 'CA', label: 'CA' },
